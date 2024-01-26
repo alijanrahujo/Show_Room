3d04b76133e71c9c46e09db6f50c8bab46d57d82
Ali Jan <alijan.rahujo143@gmail.com>
2024-01-26 18:53:17 +0500

    helper

---
+++ app/Helper/general_helper.php
@@ -0,0 +1,44 @@
+<?php
+
+if (!function_exists('status')) {
+	function status($val)
+	{
+		if ($val == 0) {
+            return "Deactivate";
+		} else if ($val == 1) {
+			return "Activate";
+		}
+        else if ($val == 2) {
+			return "Sell IN";
+		}
+        else if ($val == 3) {
+			return "Sell Out";
+		}
+        else if ($val == 4) {
+			return "Pending";
+		}
+        else if ($val == 5) {
+			return "Paid";
+		}
+        else if ($val == 6) {
+			return "Complete";
+		}
+	}
+}
+
+
+if (!function_exists('short_date')) {
+	function short_date($val)
+	{
+		return date('d-m-Y', strtotime($val));
+	}
+}
+
+if (!function_exists('long_date')) {
+	function long_date($val)
+	{
+		return date('d-m-Y h:i:s A', strtotime($val));
+	}
+}
+
+
